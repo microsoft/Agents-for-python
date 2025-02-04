132ca71994b055452a37103b65d729a385738baf
Axel Suarez <axsuarez@microsoft.com>
2025-02-03 16:32:30 -0800

    WIP client library - formatting

--- libraries/Client/microsoft-agents-client/microsoft/agents/client/channel_host_protocol.py
+++ libraries/Client/microsoft-agents-client/microsoft/agents/client/channel_host_protocol.py
@@ -8,18 +8,17 @@ class ChannelHostProtocol(Protocol):
     @property
     def host_endpoint(self) -> str:
         raise NotImplementedError()
-    
+
     @property
     def host_app_id(self) -> str:
         raise NotImplementedError()
-    
+
     @property
     def channels(self) -> dict[str, ChannelInfoProtocol]:
         raise NotImplementedError()
-    
+
     def get_channel(self, channel_info: ChannelInfoProtocol) -> ChannelProtocol:
         raise NotImplementedError()
-    
+
     def get_channel(self, name: str) -> ChannelProtocol:
         raise NotImplementedError()
-    
--- libraries/Client/microsoft-agents-client/microsoft/agents/client/channel_protocol.py
+++ libraries/Client/microsoft-agents-client/microsoft/agents/client/channel_protocol.py
@@ -1,5 +1,6 @@
 from typing import Protocol
 
+
 class ChannelProtocol(Protocol):
     async def post_activity(self, to_bot_id: str) -> dict:
         raise NotImplementedError()
