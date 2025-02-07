eb6e6cf37beb61a047b045b668d99ffba0575473
Axel Suarez <axsuarez@microsoft.com>
2025-02-06 19:08:08 -0800

    WIP: just missing ConversationIdFactory

--- libraries/Client/microsoft-agents-client/microsoft/agents/client/channel_info_protocol.py
+++ libraries/Client/microsoft-agents-client/microsoft/agents/client/channel_info_protocol.py
@@ -2,50 +2,9 @@ from typing import Protocol
 
 
 class ChannelInfoProtocol(Protocol):
-    @property
-    def id(self) -> str:
-        raise NotImplementedError()
-
-    @property.setter
-    def id(self, value: str):
-        raise NotImplementedError()
-
-    @property
-    def app_id(self) -> str:
-        raise NotImplementedError()
-
-    @property.setter
-    def app_id(self, value: str):
-        raise NotImplementedError()
-
-    @property
-    def resource_url(self) -> str:
-        raise NotImplementedError()
-
-    @property.setter
-    def resource_url(self, value: str):
-        raise NotImplementedError()
-
-    @property
-    def token_provider(self) -> str:
-        raise NotImplementedError()
-
-    @property.setter
-    def token_provider(self, value: str):
-        raise NotImplementedError()
-
-    @property
-    def channel_factory(self) -> str:
-        raise NotImplementedError()
-
-    @property.setter
-    def channel_factory(self, value: str):
-        raise NotImplementedError()
-
-    @property
-    def endpoint(self) -> str:
-        raise NotImplementedError()
-
-    @property.setter
-    def endpoint(self, value: str):
-        raise NotImplementedError()
+    id: str
+    app_id: str
+    resource_url: str
+    token_provider: str
+    channel_factory: str
+    endpoint: str
---
+++ libraries/Client/microsoft-agents-client/microsoft/agents/client/channels_configuration.py
@@ -0,0 +1,25 @@
+from typing import Protocol
+
+from .channel_info_protocol import ChannelInfoProtocol
+
+class ChannelInfo(ChannelInfoProtocol):
+    
+    def __init__(self, id: str = None, app_id: str = None, resource_url: str = None, token_provider: str = None, channel_factory: str = None, endpoint: str = None, **kwargs):
+        self.id = id
+        self.app_id = app_id
+        self.resource_url = resource_url
+        self.token_provider = token_provider
+        self.channel_factory = channel_factory
+        self.endpoint = endpoint
+
+class ChannelHostConfiguration(Protocol):
+
+    CHANNELS: list[ChannelInfoProtocol]
+    HOST_ENDPOINT: str
+    HOST_APP_ID: str
+
+class ChannelsConfiguration(Protocol):
+
+    @staticmethod
+    def CHANNEL_HOST_CONFIGURATION() -> ChannelHostConfiguration:
+        pass
