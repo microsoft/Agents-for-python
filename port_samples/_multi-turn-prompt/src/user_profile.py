75147b7d1aac4843e05bd2ff82048baf650fa23b
Rodrigo Brandão <robrandao@microsoft.com>
2025-08-04 12:03:11 -0700

    Added default values for two fields used in Activity's and removed unnecessary files

--- port_samples/_multi-turn-prompt/src/user_profile.py
+++
@@ -1,8 +0,0 @@
-from microsoft.agents.activity import Attachment
-
-class UserProfile:
-    def __init__(self, transport: str = "", name: str = "", age: int = 0, picture: Attachment = None):
-        self.transport = transport
-        self.name = name
-        self.age = age
-        self.picture = picture
