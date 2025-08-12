ece33e157358399c9d4b7b57ae0829f9adfbd8de
Axel Suárez <axsuarez@microsoft.com>
2025-08-12 11:33:31 -0700

    Users/axsuarez/enable no config (#88)
    * no config agent WIP
    
    * no config agent WIP. testing impact on other scenarios

--- test_samples/app_style/emtpy_agent.py
+++ test_samples/app_style/emtpy_agent.py
@@ -1,92 +1,40 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import re
-import sys
-import traceback
-from aiohttp.web import Application, Request, Response, run_app
-from dotenv import load_dotenv
-
-from os import environ, path
-from microsoft.agents.hosting.aiohttp import (
-    CloudAdapter,
-    jwt_authorization_middleware,
-    start_agent_process,
-)
 from microsoft.agents.hosting.core import (
-    Authorization,
     AgentApplication,
     TurnState,
     TurnContext,
     MemoryStorage,
 )
-from microsoft.agents.authentication.msal import MsalConnectionManager
-from microsoft.agents.activity import load_configuration_from_env
+from microsoft.agents.hosting.aiohttp import CloudAdapter
 
-load_dotenv(path.join(path.dirname(__file__), ".env"))
-
-agents_sdk_config = load_configuration_from_env(environ)
-
-STORAGE = MemoryStorage()
-CONNECTION_MANAGER = MsalConnectionManager(**agents_sdk_config)
-ADAPTER = CloudAdapter(connection_manager=CONNECTION_MANAGER)
-AUTHORIZATION = Authorization(STORAGE, CONNECTION_MANAGER, **agents_sdk_config)
+from shared import start_server
 
 AGENT_APP = AgentApplication[TurnState](
-    storage=STORAGE, adapter=ADAPTER, authorization=AUTHORIZATION, **agents_sdk_config
+    storage=MemoryStorage(), adapter=CloudAdapter()
 )
 
 
-@AGENT_APP.conversation_update("membersAdded")
-async def on_members_added(context: TurnContext, _state: TurnState):
+async def _help(context: TurnContext, _state: TurnState):
     await context.send_activity(
-        "Welcome to the empty agent! "
-        "This agent is designed to be a starting point for your own agent development."
+        "Welcome to the Echo Agent sample 🚀. "
+        "Type /help for help or send a message to see the echo feature in action."
     )
-    return True
 
 
-@AGENT_APP.message(re.compile(r"^hello$"))
-async def on_hello(context: TurnContext, _state: TurnState):
-    await context.send_activity("Hello!")
+AGENT_APP.conversation_update("membersAdded")(_help)
+
+AGENT_APP.message("/help")(_help)
 
 
 @AGENT_APP.activity("message")
-async def on_message(context: TurnContext, _state: TurnState):
+async def on_message(context: TurnContext, _):
     await context.send_activity(f"you said: {context.activity.text}")
 
 
-@AGENT_APP.error
-async def on_error(context: TurnContext, error: Exception):
-    # This check writes out errors to console log .vs. app insights.
-    # NOTE: In production environment, you should consider logging this to Azure
-    #       application insights.
-    print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
-    traceback.print_exc()
-
-    # Send a message to the user
-    await context.send_activity("The bot encountered an error or bug.")
-
-
-# Listen for incoming requests on /api/messages
-async def messages(req: Request) -> Response:
-    agent: AgentApplication = req.app["agent_app"]
-    adapter: CloudAdapter = req.app["adapter"]
-    return await start_agent_process(
-        req,
-        agent,
-        adapter,
-    )
-
-
-APP = Application(middlewares=[jwt_authorization_middleware])
-APP.router.add_post("/api/messages", messages)
-APP["agent_configuration"] = CONFIG
-APP["agent_app"] = AGENT_APP
-APP["adapter"] = ADAPTER
-
 if __name__ == "__main__":
     try:
-        run_app(APP, host="localhost", port=CONFIG.PORT)
+        start_server(AGENT_APP, None)
     except Exception as error:
         raise error
