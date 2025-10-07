9e7fdfa791ff0cdc7bdd5a32d5fec49c30b32326
Rodrigo Brandao <robrandao@microsoft.com>
2025-10-07 08:17:13 -0700

    Adding copyright comment

--- libraries/microsoft-agents-hosting-core/microsoft_agents/hosting/core/app/_routes/__init__.py
+++ libraries/microsoft-agents-hosting-core/microsoft_agents/hosting/core/app/_routes/__init__.py
@@ -1,7 +1,5 @@
-"""
-Copyright (c) Microsoft Corporation. All rights reserved.
-Licensed under the MIT License.
-"""
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
 
 from ._route_list import _RouteList
 from ._route import _Route, _agentic_selector
