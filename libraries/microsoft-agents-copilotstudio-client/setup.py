d3c2e421d2a62b88fac83195cec8fd95f2ff5586
Chris Mullins <cleemullins@users.noreply.github.com>
2025-10-09 10:43:13 -0700

    Add readme specification to pyproject.toml for all libraries and remove long description from setup.py

--- libraries/microsoft-agents-copilotstudio-client/setup.py
+++ libraries/microsoft-agents-copilotstudio-client/setup.py
@@ -1,9 +1,6 @@
 from os import environ
 from setuptools import setup
-from pathlib import Path
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "readme.md").read_text()
 
 package_version = environ.get("PackageVersion", "0.0.0")
 
@@ -12,6 +9,4 @@ setup(
     install_requires=[
         f"microsoft-agents-hosting-core=={package_version}",
     ],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
 )
