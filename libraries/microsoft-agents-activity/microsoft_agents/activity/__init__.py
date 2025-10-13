26f1704089e9d491762f3deb6146e9d516e6bd9a
Rodrigo Brandao <robrandao@microsoft.com>
2025-10-13 13:32:45 -0700

    Adding copyright comment

--- libraries/microsoft-agents-activity/microsoft_agents/activity/__init__.py
+++ libraries/microsoft-agents-activity/microsoft_agents/activity/__init__.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 from .agents_model import AgentsModel
 from .action_types import ActionTypes
 from .activity import Activity
--- libraries/microsoft-agents-activity/microsoft_agents/activity/entity/__init__.py
+++ libraries/microsoft-agents-activity/microsoft_agents/activity/entity/__init__.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 from .mention import Mention
 from .entity import Entity
 from .entity_types import EntityTypes
