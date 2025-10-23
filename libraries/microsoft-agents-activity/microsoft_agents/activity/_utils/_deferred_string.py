86c33ae4c06668821f0b5e809c3bb14b1bf98374
Rodrigo Brandao <robrandao@microsoft.com>
2025-10-23 14:27:28 -0700

    Adding error handling to avoid raising exceptions from deferred strings

--- libraries/microsoft-agents-activity/microsoft_agents/activity/_utils/_deferred_string.py
+++ libraries/microsoft-agents-activity/microsoft_agents/activity/_utils/_deferred_string.py
@@ -1,6 +1,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class _DeferredString:
     def __init__(self, func, *args, **kwargs):
@@ -9,4 +13,8 @@ class _DeferredString:
         self.kwargs = kwargs
 
     def __str__(self):
-        return str(self.func(*self.args, **self.kwargs))
+        try:
+            return str(self.func(*self.args, **self.kwargs))
+        except Exception as e:
+            logger.error("Error evaluating deferred string", exc_info=e)
+            return "_DeferredString: error evaluating deferred string"
