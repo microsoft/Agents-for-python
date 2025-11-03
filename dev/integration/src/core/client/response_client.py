c096048d642e0382020b8a586f3504cd0bd299ab
Rodrigo Brandao <robrandao@microsoft.com>
2025-11-03 08:51:53 -0800

    Hosting server for response client

--- dev/integration/src/core/client/response_client.py
+++ dev/integration/src/core/client/response_client.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 import sys
 from io import StringIO
-from threading import Lock
+from threading import Lock, to_thread, Thread
 import asyncio
 
 from aiohttp import ClientSession
-from aiohttp.web import Application, Request, Response
+from aiohttp.web import Application, Request, Response, run_app
 
 from microsoft_agents.activity import (
     Activity,
@@ -17,13 +17,20 @@ class ResponseClient:
     
     def __init__(
         self,
-        service_endpoint: str = "http://localhost:9873"
+        host: str = "localhost",
+        port: int = 9873,
     ):
         self._app: Application = Application()
         self._prev_stdout = None
+        service_endpoint = f"{host}:{port}"
+        self._host = host
+        self._port = port
+        if "http" not in service_endpoint:
+            service_endpoint = f"http://{service_endpoint}"
         self._service_endpoint = service_endpoint
         self._activities_list = []
         self._activities_list_lock = Lock()
+        self._server_thread: Optional[Thread] = None
 
         self._app.router.add_post(
             "/v3/conversations/{path:.*}",
@@ -33,15 +40,28 @@ class ResponseClient:
     @property
     def service_endpoint(self) -> str:
         return self._service_endpoint
+    
+    def start(self) -> None:
+        run_app(self._app, host=self._host, port=self._port)
 
     async def __aenter__(self) -> ResponseClient:
+        if self._server_thread:
+            raise RuntimeError("ResponseClient is already running.")
+        
         self._prev_stdout = sys.stdout
         sys.stdout = StringIO()
+        self._server_thread = Thread(target=self.start)
+        self._server_thread.start()
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
+        if not self._server_thread:
+            raise RuntimeError("ResponseClient is not running.")
         if self._prev_stdout is not None:
             sys.stdout = self._prev_stdout
+
+        self._server_thread.join()
+        self._server_thread = None
 
     async def _handle_conversation(self, request: Request) -> Response:
         try:
--- dev/integration/src/tests/core/client/test_response_client.py
+++ dev/integration/src/tests/core/client/test_response_client.py
@@ -1,4 +1,3 @@
-from re import A
 import pytest
 from aiohttp import ClientSession
 
