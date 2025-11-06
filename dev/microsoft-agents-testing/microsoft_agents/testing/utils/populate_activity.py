5eb9bfb4d495f78db6bb0d3742d296e79dea42d3
Rodrigo Brandao <robrandao@microsoft.com>
2025-11-06 11:29:59 -0800

    Removing unused code

--- dev/microsoft-agents-testing/microsoft_agents/testing/utils/populate_activity.py
+++ dev/microsoft-agents-testing/microsoft_agents/testing/utils/populate_activity.py
@@ -2,6 +2,7 @@ from microsoft_agents.activity import Activity
 
 
 def populate_activity(original: Activity, defaults: Activity | dict) -> Activity:
+    """Populate an Activity object with default values."""
 
     if isinstance(defaults, Activity):
         defaults = defaults.model_dump(exclude_unset=True)
@@ -13,31 +14,3 @@ def populate_activity(original: Activity, defaults: Activity | dict) -> Activity
             setattr(new_activity, key, defaults[key])
 
     return new_activity
-
-
-# def _populate_incoming_activity(activity: Activity) -> Activity:
-
-#     activity = activity.model_copy()
-
-#     if not activity.locale:
-#         activity.locale = "en-US"
-
-#     if not activity.channel_id:
-#         activity.channel_id = "emulator"
-
-#     if not activity.delivery_mode:
-#         activity.delivery_mode = DeliveryModes.normal
-
-#     if not activity.service_url:
-#         activity.service_url = "http://localhost"
-
-#     if not activity.recipient:
-#         activity.recipient = ChannelAccount(id="agent", name="Agent")
-
-#     if not activity.from_property:
-#         activity.from_property = ChannelAccount(id="user", name="User")
-
-#     if not activity.conversation:
-#         activity.conversation = ConversationAccount(id="conversation1")
-
-#     return activity
