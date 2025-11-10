bcfb70ecbc71d444e6fcac21b9dc69c1172bdcb3
Rodrigo Brandao <robrandao@microsoft.com>
2025-11-10 14:38:26 -0800

    Sketch of RestChannelServiceClientFactory test

--- tests/hosting_core/test_rest_channel_service_client_factory.py
+++ tests/hosting_core/test_rest_channel_service_client_factory.py
@@ -1,15 +1,80 @@
-import pytest
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
 
+import pytest
+from unittest.mock import AsyncMock, Mock, MagicMock
+
+from microsoft_agents.activity import Activity, RoleTypes, ChannelAccount
 from microsoft_agents.hosting.core import (
     RestChannelServiceClientFactory,
+    TurnContext,
 )
+from microsoft_agents.hosting.core.authorization import (
+    AuthenticationConstants,
+    ClaimsIdentity,
+    Connections,
+    AccessTokenProviderBase,
+)
+from microsoft_agents.hosting.core.connector.teams import TeamsConnectorClient
+from microsoft_agents.hosting.core.connector.client import UserTokenClient
+
+from tests._common.data import DEFAULT_TEST_VALUES
+
+DEFAULTS = DEFAULT_TEST_VALUES()
+
 
 class TestRestChannelServiceClientFactory:
+
     
+    @pytest.mark.parametrize(
+        "token_service_endpoint, token_service_audience",
+        [
+            (AuthenticationConstants.AGENTS_SDK_OAUTH_URL, AuthenticationConstants.AGENTS_SDK_SCOPE),
+            ("https://custom.token.endpoint", "https://custom.token.audience"),
+        ]
+    )
     @pytest.mark.asyncio
-    def test_create_connector_client(self):
+    async def test_create_connector_client_anonymous(
+        self, 
+        mocker,
+        token_service_endpoint,
+        token_service_audience
+    ):
+        # setup
+        token_provider = mocker.mock(spec=AccessTokenProviderBase)
+        token_provider.get_access_token = AsyncMock(return_value=DEFAULTS.token)
+
+        connection_manager = mocker.mock(spec=Connections)
+        connection_manager.get_token_provider = mocker.Mock(
+            return_value=token_provider
+        )
+
+        factory = RestChannelServiceClientFactory(
+            connection_manager,
+            token_service_endpoint,
+            token_service_audience
+        )
+
+        activity = Activity(
+            type="message",
+        )
         
-        factory = RestChannelServiceClientFactory()
-        client = factory.create_connector_client("http://example.com")
-        assert client is not None
-        assert client.base_url == "http://example.com"
+        claims_identity = ClaimsIdentity(
+            {
+
+            },
+
+        )
+
+        context = TurnContext(
+
+        )
+
+        res = await factory.create_connector_client(
+            context,
+            claims_identity,
+            service_url,
+            audience,
+            scopes,
+            use_anynoymous=True,
+        )
