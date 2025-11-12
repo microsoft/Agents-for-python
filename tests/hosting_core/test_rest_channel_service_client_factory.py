6527ec704b53e009fbd5b45b7483ee1c1a932690
Rodrigo Brandao <robrandao@microsoft.com>
2025-11-12 08:06:56 -0800

    Adding create_connector_client calls that use agentic identity

--- tests/hosting_core/test_rest_channel_service_client_factory.py
+++ tests/hosting_core/test_rest_channel_service_client_factory.py
@@ -14,6 +14,7 @@ from microsoft_agents.hosting.core.authorization import (
     Connections,
     AccessTokenProviderBase,
     AnonymousTokenProvider,
+    AgentAuthConfiguration,
 )
 from microsoft_agents.hosting.core.connector.teams import TeamsConnectorClient
 from microsoft_agents.hosting.core.connector.client import UserTokenClient
@@ -42,37 +43,42 @@ class TestRestChannelServiceClientFactory:
     def context_flag(self, request):
         return request.param
 
-    # @pytest.fixture
-    # def activity_agentic_user(self):
-    #     return Activity(
-    #         type="message",
-    #         channel_id="msteams",
-    #         from_property=ChannelAccount(id="agentic_user", role=RoleTypes.USER),
-    #         recipient=ChannelAccount(id="bot1", role=RoleTypes.BOT),
-    #         service_url="https://service.url/",
-    #         conversation={"id": "conv1"},
-    #         id="activity_agentic1",
-    #         text="Hello, World!",
-    #         properties={
-    #             "agenticRequest": True
-    #         }
-    #     )
+    @pytest.fixture
+    def activity_agentic_user(self):
+        return Activity(
+            type="message",
+            channel_id="msteams",
+            from_property=ChannelAccount(id="agentic_user", role=RoleTypes.user),
+            recipient=ChannelAccount(
+                id="bot1",
+                agentic_app_id="agentic_app_id",
+                agentic_user_id="agentic_user_id",
+                role=RoleTypes.agentic_user,
+            ),
+            service_url="https://service.url/",
+            conversation={"id": "conv1"},
+            id="activity_agentic1",
+            text="Hello, World!",
+            properties={"agenticRequest": True},
+        )
 
-    # @pytest.fixture
-    # def activity_agentic_identity(self):
-    #    return Activity(
-    #         type="message",
-    #         channel_id="msteams",
-    #         from_property=ChannelAccount(id="agentic_user", role=RoleTypes.USER),
-    #         recipient=ChannelAccount(id="bot1", role=RoleTypes.BOT),
-    #         service_url="https://service.url/",
-    #         conversation={"id": "conv1"},
-    #         id="activity_agentic1",
-    #         text="Hello, World!",
-    #         properties={
-    #             "agenticRequest": True
-    #         }
-    #     )
+    @pytest.fixture
+    def activity_agentic_identity(self):
+        return Activity(
+            type="message",
+            channel_id="msteams",
+            from_property=ChannelAccount(id="agentic_user", role=RoleTypes.user),
+            recipient=ChannelAccount(
+                id="bot1",
+                agentic_app_id="agentic_app_id",
+                role=RoleTypes.agentic_identity,
+            ),
+            service_url="https://service.url/",
+            conversation={"id": "conv1"},
+            id="activity_agentic1",
+            text="Hello, World!",
+            properties={"agenticRequest": True},
+        )
 
     @pytest.mark.parametrize(
         "token_service_endpoint, token_service_audience",
@@ -250,6 +256,132 @@ class TestRestChannelServiceClientFactory:
         )
         assert token_provider.get_access_token.call_count == 1
         token_provider.get_access_token.assert_called_once_with(audience, scopes)
+        TeamsConnectorClient.__new__.assert_called_once_with(
+            TeamsConnectorClient, endpoint=DEFAULTS.service_url, token=DEFAULTS.token
+        )
+
+    @pytest.mark.parametrize("alt_blueprint", [True, False])
+    @pytest.mark.asyncio
+    async def test_create_connector_client_agentic_identity(
+        self, mocker, activity_agentic_identity, alt_blueprint
+    ):
+        # setup
+        mock_connector_client = mocker.Mock(spec=TeamsConnectorClient)
+        mocker.patch.object(
+            TeamsConnectorClient, "__new__", return_value=mock_connector_client
+        )
+
+        token_provider = mocker.Mock(spec=AccessTokenProviderBase)
+        token_provider.get_agentic_instance_token = mocker.AsyncMock(
+            return_value=(DEFAULTS.token, None)
+        )
+
+        connection_manager = mocker.Mock(spec=Connections)
+        connection_manager.get_token_provider = mocker.Mock(return_value=token_provider)
+
+        auth_config = AgentAuthConfiguration()
+        if alt_blueprint:
+            auth_config.ALT_BLUEPRINT_ID = "alt_blueprint_id"
+            connection_manager.get_connection = mocker.Mock(return_value=token_provider)
+        token_provider._msal_configuration = auth_config
+
+        factory = RestChannelServiceClientFactory(connection_manager)
+
+        claims_identity = mocker.Mock(spec=ClaimsIdentity)
+        service_url = DEFAULTS.service_url
+        audience = "https://service.audience/"
+        scopes = ["scope1", "scope2"]
+
+        context = mocker.Mock(spec=TurnContext)
+        context.activity = activity_agentic_identity
+
+        # test
+
+        res = await factory.create_connector_client(
+            context,
+            claims_identity,
+            service_url,
+            audience,
+            scopes,
+        )
+
+        # verify
+        assert connection_manager.get_token_provider.call_count == 1
+        connection_manager.get_token_provider.assert_called_once_with(
+            context.identity, service_url
+        )
+        if alt_blueprint:
+            connection_manager.get_connection.assert_called_once_with(
+                "alt_blueprint_id"
+            )
+        assert token_provider.get_agentic_instance_token.call_count == 1
+        token_provider.get_agentic_instance_token.assert_called_once_with(
+            "agentic_app_id"
+        )
+        TeamsConnectorClient.__new__.assert_called_once_with(
+            TeamsConnectorClient, endpoint=DEFAULTS.service_url, token=DEFAULTS.token
+        )
+
+    @pytest.mark.parametrize("alt_blueprint", [True, False])
+    @pytest.mark.asyncio
+    async def test_create_connector_client_agentic_user(
+        self, mocker, activity_agentic_user, alt_blueprint
+    ):
+        # setup
+        mock_connector_client = mocker.Mock(spec=TeamsConnectorClient)
+        mocker.patch.object(
+            TeamsConnectorClient, "__new__", return_value=mock_connector_client
+        )
+
+        token_provider = mocker.Mock(spec=AccessTokenProviderBase)
+        token_provider.get_agentic_user_token = mocker.AsyncMock(
+            return_value=DEFAULTS.token
+        )
+
+        connection_manager = mocker.Mock(spec=Connections)
+        connection_manager.get_token_provider = mocker.Mock(return_value=token_provider)
+
+        auth_config = AgentAuthConfiguration()
+        if alt_blueprint:
+            auth_config.ALT_BLUEPRINT_ID = "alt_blueprint_id"
+            connection_manager.get_connection = mocker.Mock(return_value=token_provider)
+        token_provider._msal_configuration = auth_config
+
+        factory = RestChannelServiceClientFactory(connection_manager)
+
+        claims_identity = mocker.Mock(spec=ClaimsIdentity)
+        service_url = DEFAULTS.service_url
+        audience = "https://service.audience/"
+        scopes = ["scope1", "scope2"]
+
+        context = mocker.Mock(spec=TurnContext)
+        context.activity = activity_agentic_user
+
+        # test
+
+        res = await factory.create_connector_client(
+            context,
+            claims_identity,
+            service_url,
+            audience,
+            scopes,
+        )
+
+        # verify
+        assert connection_manager.get_token_provider.call_count == 1
+        connection_manager.get_token_provider.assert_called_once_with(
+            context.identity, service_url
+        )
+        if alt_blueprint:
+            connection_manager.get_connection.assert_called_once_with(
+                "alt_blueprint_id"
+            )
+        assert token_provider.get_agentic_user_token.call_count == 1
+        token_provider.get_agentic_user_token.assert_called_once_with(
+            "agentic_app_id",
+            "agentic_user_id",
+            [AuthenticationConstants.APX_PRODUCTION_SCOPE],
+        )
         TeamsConnectorClient.__new__.assert_called_once_with(
             TeamsConnectorClient, endpoint=DEFAULTS.service_url, token=DEFAULTS.token
         )
