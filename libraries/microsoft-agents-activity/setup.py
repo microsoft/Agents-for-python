12ac42955e34d85839f26e7a1373a5d1ba72306d
Rodrigo Brandao <robrandao@microsoft.com>
2025-11-26 14:41:20 -0800

    Specifying encoding

--- libraries/microsoft-agents-activity/setup.py
+++ libraries/microsoft-agents-activity/setup.py
@@ -2,10 +2,9 @@ from os import environ, path
 from setuptools import setup
 
 # Try to read from VERSION.txt file first, fall back to environment variable
-package_version = "0.0.0"
 version_file = path.join(path.dirname(__file__), "VERSION.txt")
 if path.exists(version_file):
-    with open(version_file, "r") as f:
+    with open(version_file, "r", encoding="utf-8") as f:
         package_version = f.read().strip()
 else:
     package_version = environ.get("PackageVersion", "0.0.0")
