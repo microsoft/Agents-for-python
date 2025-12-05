7e01415b6f32c60ccb4fa42c170ffdbeb7094548
Rodrigo Brandao <robrandao@microsoft.com>
2025-12-05 09:12:08 -0800

    Adding extra documentation

--- libraries/microsoft-agents-copilotstudio-client/microsoft_agents/copilotstudio/client/connection_settings.py
+++ libraries/microsoft-agents-copilotstudio-client/microsoft_agents/copilotstudio/client/connection_settings.py
@@ -15,10 +15,22 @@ class ConnectionSettings(DirectToEngineConnectionSettingsProtocol):
         self,
         environment_id: str,
         agent_identifier: str,
-        cloud: Optional[PowerPlatformCloud],
-        copilot_agent_type: Optional[AgentType],
-        custom_power_platform_cloud: Optional[str],
+        cloud: Optional[PowerPlatformCloud] = None,
+        copilot_agent_type: Optional[AgentType] = None,
+        custom_power_platform_cloud: Optional[str] = None,
+        client_session_kwargs: Optional[dict] = None,
     ) -> None:
+        """Initialize connection settings.
+
+        :param environment_id: The ID of the environment to connect to.
+        :param agent_identifier: The identifier of the agent to use for the connection.
+        :param cloud: The PowerPlatformCloud to use for the connection.
+        :param copilot_agent_type: The AgentType to use for the Copilot.
+        :param custom_power_platform_cloud: The custom PowerPlatformCloud URL.
+        :param client_session_kwargs: Additional keyword arguments for initialization
+            of the underlying Aiohttp ClientSession.
+        """
+
         self.environment_id = environment_id
         self.agent_identifier = agent_identifier
 
@@ -30,3 +42,4 @@ class ConnectionSettings(DirectToEngineConnectionSettingsProtocol):
         self.cloud = cloud or PowerPlatformCloud.PROD
         self.copilot_agent_type = copilot_agent_type or AgentType.PUBLISHED
         self.custom_power_platform_cloud = custom_power_platform_cloud
+        self.client_session_kwargs = client_session_kwargs or {}
