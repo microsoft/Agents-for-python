54c4981255f2f9ecf2b394263fd0640813c4839f
Rodrigo Brandao <robrandao@microsoft.com>
2026-01-05 09:58:32 -0800

    Porting test cases

--- dev/tests/E2E/basic_agent/test_basic_agent.py
+++ dev/tests/E2E/basic_agent/test_basic_agent.py
@@ -10,7 +10,7 @@ TEST_BASIC_AGENT = True
 @pytest.mark.skipif(
     not TEST_BASIC_AGENT, reason="Skipping external agent tests for now."
 )
-class TestBasicAgentExternal(Integration):
+class TestBasicAgent(Integration):
     _agent_url = "http://localhost:3978/"
     _service_url = "http://localhost:8001/"
     _config_path = "agents/basic_agent/python/.env"
