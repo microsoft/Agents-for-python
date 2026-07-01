7418af18b1d03faaef96062f33beb82e68dc7065
kylerohn-msft <kylerohn@microsoft.com>
2026-07-01 13:42:36 -0700

    create colorful logging

--- libraries/microsoft-agents-activity/microsoft_agents/activity/config/_configure_logging.py
+++ libraries/microsoft-agents-activity/microsoft_agents/activity/config/_configure_logging.py
@@ -20,6 +20,36 @@ _NAME_TO_LEVEL = {
 }
 
 
+class ColorFormatter(logging.Formatter):
+    """Custom logging formatter that adds color to log messages based on their severity level."""
+
+    BRIGHT_RED = "\033[1;31m"
+    RED = "\033[31m"
+    YELLOW = "\033[33m"
+    GREEN = "\033[32m"
+    BLUE = "\033[34m"
+    RESET = "\033[0m"
+
+    log_format = (
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    )
+
+    FORMATS = {
+        logging.CRITICAL: f"{BRIGHT_RED}{log_format}{RESET}",
+        logging.ERROR: f"{RED}{log_format}{RESET}",
+        logging.WARNING: f"{YELLOW}{log_format}{RESET}",
+        logging.INFO: f"{GREEN}{log_format}{RESET}",
+        logging.DEBUG: f"{BLUE}{log_format}{RESET}",
+        logging.NOTSET: f"{RESET}{log_format}{RESET}",
+    }
+
+    def format(self, record):
+        """Formats the log record with color based on its severity level."""
+        log_fmt = self.FORMATS.get(record.levelno, self.FORMATS[logging.NOTSET])
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 def _configure_logging(logging_config: dict):
     """Configures logging based on the provided logging configuration dictionary.
 
@@ -30,11 +60,7 @@ def _configure_logging(logging_config: dict):
     log_levels = logging_config.get("LOGLEVEL", {})
 
     console_handler = logging.StreamHandler()
-    console_handler.setFormatter(
-        logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
-        )
-    )
+    console_handler.setFormatter(ColorFormatter())
 
     for key in log_levels:
         level_name = log_levels[key].upper()
@@ -50,6 +76,7 @@ def _configure_logging(logging_config: dict):
         else:
             logger = logging.getLogger(namespace)
 
+        logger.propagate = False  # Prevent log messages from being propagated
         logger.handlers.clear()  # Remove existing handlers to prevent duplicates
         logger.addHandler(console_handler)
         logger.setLevel(level)
