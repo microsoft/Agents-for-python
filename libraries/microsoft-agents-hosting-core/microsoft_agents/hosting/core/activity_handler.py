a0da7ff0326b96fc2e6f92c755ff696ccdb12d19
Rodrigo Brandao <robrandao@microsoft.com>
2026-07-10 09:40:56 -0700

    Removing unused imports

--- libraries/microsoft-agents-hosting-core/microsoft_agents/hosting/core/activity_handler.py
+++ libraries/microsoft-agents-hosting-core/microsoft_agents/hosting/core/activity_handler.py
@@ -3,7 +3,6 @@
 
 from __future__ import annotations
 from http import HTTPStatus
-from typing import Awaitable
 from pydantic import BaseModel
 
 from microsoft_agents.activity import TurnContextProtocol
