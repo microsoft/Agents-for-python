b60649cde10421278a187b1a0aeb8d5b7dc6f9dd
Rodrigo Brandao <robrandao@microsoft.com>
2026-07-30 14:25:22 -0700

    Removing mocks

--- dev/microsoft-agents-testing/microsoft_agents/testing/mocks/mock_cloud_adapter.py
+++
@@ -1,6 +0,0 @@
-from microsoft_agents.hosting.core import (
-    ChannelServiceAdapter,
-    RestChannelServiceClientFactory,
-)
-
-def mock_cloud_adapter(adapter_cls: type[HttpAdapterBase]) -> Callable[]
