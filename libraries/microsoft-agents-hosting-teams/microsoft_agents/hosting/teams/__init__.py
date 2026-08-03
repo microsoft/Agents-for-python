ac5ba65df4b3f3de46d4a7d3f26468c7b2e6ed2b
Rodrigo Brandão <robrandao@microsoft.com>
2026-08-03 14:09:09 -0700

    Add deprecated warning on `microsoft-agents-hosting-teams` package import (#517)
    * Obsolete warning for hosting-teams
    
    * Update
    
    * Update

--- libraries/microsoft-agents-hosting-teams/microsoft_agents/hosting/teams/__init__.py
+++ libraries/microsoft-agents-hosting-teams/microsoft_agents/hosting/teams/__init__.py
@@ -1,3 +1,16 @@
+"""
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+"""
+
+import logging
+
+logging.warning(
+    "The `microsoft-agents-hosting-teams` package is deprecated. "
+    "Please start using the `microsoft-agents-hosting-msteams` package "
+    "for better support and future updates. "
+)
+
 from .teams_activity_handler import TeamsActivityHandler
 from .teams_agent_extension import (
     TeamsAgentExtension,
