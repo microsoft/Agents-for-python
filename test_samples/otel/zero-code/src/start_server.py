5329d21a75bd50243531e2be76a23c55dc2e077e
Rodrigo Brandao <robrandao@microsoft.com>
2026-08-21 10:32:16 -0700

    Typing span definition and new zero-code OTEL sample

---
+++ test_samples/otel/zero-code/src/start_server.py
@@ -0,0 +1,32 @@
+import logging
+from os import environ
+
+from aiohttp.web import Application, Request, Response, run_app
+from microsoft_agents.hosting.aiohttp import (
+    CloudAdapter,
+    jwt_authorization_middleware,
+    start_agent_process,
+)
+from microsoft_agents.hosting.core import AgentApplication, AgentAuthConfiguration
+
+logger = logging.getLogger(__name__)
+
+
+def start_server(
+    agent_application: AgentApplication, auth_configuration: AgentAuthConfiguration
+):
+    async def entry_point(req: Request) -> Response:
+        logger.info("Request received at /api/messages endpoint.")
+        agent: AgentApplication = req.app["agent_app"]
+        adapter: CloudAdapter = req.app["adapter"]
+
+        return await start_agent_process(req, agent, adapter)
+
+    app = Application(middlewares=[jwt_authorization_middleware])
+    app.router.add_post("/api/messages", entry_point)
+
+    app["agent_configuration"] = auth_configuration
+    app["agent_app"] = agent_application
+    app["adapter"] = agent_application.adapter
+
+    run_app(app, host="localhost", port=int(environ.get("PORT", 3978)))
